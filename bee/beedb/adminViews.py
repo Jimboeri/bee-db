20b5f102454e0cbc3eaf30cf129dbf076f81aaf5
Jimboeri <Jimboeri@users.noreply.github.com>
2024-07-27 03:39:08 +0000

    Style fixes by ruff

--- bee/beedb/adminViews.py
+++ bee/beedb/adminViews.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 
-from django.urls import reverse
-from django.utils import timezone
-#from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django import forms
+# from django.utils.encoding import force_bytes, force_text
 
-from .models import Apiary
 
 # from .forms import (
 #    ProfileForm,
