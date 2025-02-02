cb475421e36333da199ed8d087474a8c006f47ec
Jimboeri <13359489+Jimboeri@users.noreply.github.com>
2025-02-02 00:16:36 +0000

    Style fixes by ruff

--- bee/beedb/context_processors.py
+++ bee/beedb/context_processors.py
@@ -1,17 +1,18 @@
 from django.conf import settings
 import logging
 
+
 def global_settings(request):
-    isDev = getattr(settings, "IS_DEV_ENV", False),  # Defaults to False if not set
+    isDev = (getattr(settings, "IS_DEV_ENV", False),)  # Defaults to False if not set
 
     logging.info(f"Dev setting is |{isDev}|")
     logging.info(f"Type is {type(isDev)}")
     if True in isDev:
-      envMsg = 'DEVELOPMENT'
-      matomoId = 5                  # defined on webstat.west.net.nz
+        envMsg = "DEVELOPMENT"
+        matomoId = 5  # defined on webstat.west.net.nz
     else:
-       envMsg = ''
-       matomoId = 1                 # defined on webstat.west.net.nz
+        envMsg = ""
+        matomoId = 1  # defined on webstat.west.net.nz
     return {
         "IS_DEV_ENV": isDev,
         "ENV_MSG": envMsg,
