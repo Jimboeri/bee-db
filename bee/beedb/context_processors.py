af1e7df4ae5ce19096cb74d6054c93b0f107e110
Jimboeri <jim@west.net.nz>
2025-02-07 02:52:32 +0000

    debug msg added in context proc

--- bee/beedb/context_processors.py
+++ bee/beedb/context_processors.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 import os
-
+import logging
 
 def global_settings(request):
     #isDev = (getattr(settings, "IS_DEV_ENV", False),)  # Defaults to False if not set
 
-    matomoId = os.environ.get("MATOMO_SITE_ID", '')
+    matomoId = os.environ.get("MATOMO_SITE_ID", '0')
+    logging.info(f"MATOMO_SITE_ID: {matomoId}")
 
     if matomoId ==  '5':
         envMsg = "DEVELOPMENT"
