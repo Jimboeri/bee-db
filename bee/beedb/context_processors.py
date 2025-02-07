c4b69e8ac49de154d24e579bdc100a0acb664253
Jimboeri <13359489+Jimboeri@users.noreply.github.com>
2025-02-07 02:56:04 +0000

    Style fixes by ruff

--- bee/beedb/context_processors.py
+++ bee/beedb/context_processors.py
@@ -1,10 +1,11 @@
 import os
 import logging
 
+
 def global_settings(request):
     # isDev = (getattr(settings, "IS_DEV_ENV", False),)  # Defaults to False if not set
 
-    matomoId = os.environ.get("MATOMO_SITE_ID", '0')
+    matomoId = os.environ.get("MATOMO_SITE_ID", "0")
     logging.info(f"MATOMO_SITE_ID: {matomoId}")
 
     if matomoId == "5":
