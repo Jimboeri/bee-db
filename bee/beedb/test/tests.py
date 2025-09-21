36008625504c1e2d70d61addd0420603ff5bc05c
Jimboeri <jim@west.net.nz>
2025-09-21 04:22:10 +0000

    Test improvements

---
+++ bee/beedb/test/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from beedb import models
+
+# Create your tests here.
+class TestModels(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('testuser',)
+
+    def test_Model_Usr(self):
+        self.assertTrue(True)
