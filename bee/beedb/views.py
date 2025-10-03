29433de3d326f82955d6ac49330b2fa14b0a8a12
Jimboeri <jim@west.net.nz>
2025-10-03 21:32:44 +0000

    Fix

--- bee/beedb/views.py
+++ bee/beedb/views.py
@@ -1074,7 +1074,7 @@ def apReport(request, ap_ref, duration=4):
     else:
         startDt = timezone.now() - datetime.timedelta(weeks=1000)
 
-    pastAudits = col.audit_set.filter(dt__gte=startDt)  # type: ignore
+    pastAudits = ap.colony_set.audit_set.filter(dt__gte=startDt)  # type: ignore
     context = {
         "ap": ap,
         "reportactive": "Y",
