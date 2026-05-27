7d30a652efbd182a0520d702826238556fa6015a
Jim West <jim.west.net.nz>
2026-05-27 20:30:09 +1200

    Media fix

--- bee/bee/urls.py
+++ bee/bee/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings  # new
-from django.conf.urls.static import static  # new
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("beedb/", include("beedb.urls")),
     # path('accounts/', include('django.contrib.auth.urls')),
+    # Serve media files directly from Django (works regardless of DEBUG).
+    # Acceptable for low-traffic / self-hosted deployments.
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
