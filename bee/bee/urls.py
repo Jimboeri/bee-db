51f88f725e2d1c8a4d863fd98619ae2c8710fdce
Jim West <jim.west.net.nz>
2026-05-27 20:06:20 +1200

    Media in prod

--- bee/bee/urls.py
+++ bee/bee/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     path("beedb/", include("beedb.urls")),
     # path('accounts/', include('django.contrib.auth.urls')),
 ]
-if settings.DEBUG:  # new
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
